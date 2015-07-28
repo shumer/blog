998dc0a35bfb871bfa25343b76b7d0cb4fe6bba6
Maxim Krestnikov <mkrestnicov@adyax.com>
2015-07-28 22:13:21 +0300

    Codesnippet plugin added

---
+++ modules/custom/dart_core/src/Plugin/CKEditorPlugin/Codesnippet.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\dart_core\Plugin\CKEditorPlugin\Codesnippet.
+ */
+namespace Drupal\dart_core\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "codesnippet" plugin.
+ *
+ * @CKEditorPlugin(
+ *   id = "codesnippet",
+ *   label = @Translation("Codesnippet"),
+ * )
+ */
+class Codesnippet extends CKEditorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+
+    return drupal_get_path('module', 'dart_core') . '/js/plugins/codesnippet/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    return array(
+      'codeSnippet_codeClass' =>  t('Code class'),
+      'codeSnippet_languages ' => t('Code Languages'),
+      'codeSnippet_theme' => t('Code theme'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getButtons() {
+    $path = drupal_get_path('module', 'dart_core') . '/js/plugins/codesnippet/icons/';
+    return array(
+      'CodeSnippet' => array(
+        'label' => t('Code'),
+        'image' => $path . '/codesnippet.png',
+      ),
+    );
+  }
+
+}
