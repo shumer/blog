224933c387ee59b2a88b9dd51f1b285a0bcde7c1
Alexander Shumenko <ashumenko@adyax.com>
2015-09-01 12:48:23 +0300

    Fixes.

--- modules/custom/advanced_varnish_cache/src/Controller/AdvancedVarnishCacheESIController.php
+++ modules/custom/advanced_varnish_cache/src/Controller/AdvancedVarnishCacheESIController.php
@@ -35,67 +35,17 @@ class AdvancedVarnishCacheESIController extends ControllerBase{
 
       // Render block.
       $build = \Drupal::entityManager()->getViewBuilder('block')
-        ->view($block);//var_dump($build);
+        ->view($block);
       $content = \Drupal::service('renderer')->render($build);
       $content .= date('H:i:s', time());
       $response->headers->set(AdvancedVarnishCache::ADVANCED_VARNISH_CACHE_X_TTL, $ttl);
       $response->headers->set(AdvancedVarnishCache::ADVANCED_VARNISH_CACHE_HEADER_CACHE_TAG, $tags);
-      $exp = new \DateTime();
-      $exp->setTimestamp(time() + $ttl);
-      $response->headers->set('Expires', date('j-M-Y H:i:s T', time() + $ttl));
-      $response->setExpires($exp);
     }
 
     // Set rendered block as response object content.
     $response->setContent($content);
-    $this->cookie_update();
 
-    //kpr('$response->getExpires() AvcESIController');
-    //kpr($response->getExpires());
     return $response;
   }
 
-  /**
-   * Updates cookie for ESI.
-   */
-  protected function cookie_update($account = '') {
-
-    $varnish_handler = new  AdvancedVarnishCache();
-
-    $config = \Drupal::config('advanced_varnish_cache.settings');
-    $account = $account ?: \Drupal::currentUser();
-
-    // If user should bypass varnish we must set per user bin.
-    if ($account->hasPermission('bypass advanced varnish cache')) {
-      $bin = 'u' . $account->id();
-    }
-    elseif ($account->id() > 0) {
-      $roles = $account->getRoles();
-      sort($roles);
-      $bin = implode('__', $roles);
-    }
-    else {
-      // Bin for anonym user.
-      $bin = '0';
-    }
-    $cookie_inf = $bin;
-
-    $noise = $config->get('general.noise') ?: '';
-
-    // Allow other modules to interfere.
-    \Drupal::moduleHandler()->alter('advanced_varnish_cache_user_cache_bin', $cookie_inf, $account);
-
-    // Hash bin (PER_ROLE-PER_PAGE).
-    $cookie_bin = hash('sha256', $cookie_inf . $noise) . '-' . hash('sha256', $noise);
-
-    // Update cookies if did not match.
-    if (empty($_COOKIE[$varnish_handler->getCookieBin()]) || ($_COOKIE[$varnish_handler->getCookieBin()] != $cookie_bin)) {
-
-      // Update cookies.
-      $params = session_get_cookie_params();
-      $expire = $params['lifetime'] ? (REQUEST_TIME + $params['lifetime']) : 0;
-      setcookie($varnish_handler->getCookieBin(), $cookie_bin, $expire, $params['path'], $params['domain'], FALSE, $params['httponly']);
-      setcookie($varnish_handler->getCookieInf(), $cookie_inf, $expire, $params['path'], $params['domain'], FALSE, $params['httponly']);
-    }
-  }
 }
