4552c46d90238c8989d9e111f25f1ed6b40e5d6d
Maxim Krestnikov <mkrestnicov@adyax.com>
2015-09-01 22:44:25 +0300

    Deny page cache ResponsePolicy service implemented

---
+++ modules/custom/advanced_varnish_cache/src/Plugin/Layout/CacheableLayout.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\advanced_varnish_cache\Plugin\Layout\CacheableLayout.
+ */
+
+namespace Drupal\advanced_varnish_cache\Plugin\Layout;
+
+use Drupal\layout_plugin\Plugin\Layout\LayoutBase;
+use \Drupal\Core\Render\Element;
+use Drupal\advanced_varnish_cache\AdvancedVarnishCache;
+
+class CacheableLayout extends LayoutBase {
+
+  public function build(array $regions) {
+    $varnish = new AdvancedVarnishCache();
+dpm($regions, 'regions');
+
+    foreach (Element::children($regions) as $region_id) {
+      $region = &$regions[$region_id];
+      foreach (Element::children($region) as $block_id) {
+        $block = &$region[$block_id];
+        if (!empty($block['#configuration']['cache']['esi'])) {
+
+          // If we need to replace block with ESI we change pre_render callback to handle this.
+          $block['#theme'] = 'advanced_varnish_cache_esi_block';
+          $block['#pre_render'] = [[$varnish, 'buildEsiBlock']];
+          $block['#conf_collection'] = [$block_id = $block['#configuration']];
+        }
+      }
+    }
+    return parent::build($regions);
+  }
+
+}
