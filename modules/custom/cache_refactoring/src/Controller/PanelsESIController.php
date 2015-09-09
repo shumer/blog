7a6bbdcbec4853ac9010c9c3948ff7895fcc41ed
Alexander Shumenko <ashumenko@adyax.com>
2015-09-09 17:22:06 +0300

    Panels ESI controller updated.

--- modules/custom/cache_refactoring/src/Controller/PanelsESIController.php
+++ modules/custom/cache_refactoring/src/Controller/PanelsESIController.php
@@ -15,4 +15,36 @@ use Symfony\Component\Validator\Constraints\DateTime;
 
 class PanelsESIController extends ControllerBase {
 
+  /**
+   * Return rendered block html to replace esi tag.
+   */
+  public function content($page, $block_id){
+
+    $response = new CacheableResponse();
+
+    $conf = \Drupal::config("page_manager.page.$page")->get('display_variants');
+
+    foreach ($conf as $display_variant) {
+      if (isset($display_variant['blocks'][$block_id])) {
+        $block_conf = $display_variant['blocks'][$block_id];
+      }
+    }
+    $block = \Drupal::service('plugin.manager.block')->createInstance($block_conf['id'], $block_conf);
+    $build = $block->build();
+    $content = \Drupal::service('renderer')->renderPlain($build);
+    if ($block) {
+
+      // Mark this block and response as rendered through ESI request.
+      $block->_esi = 1;
+      $response->_esi = 1;
+
+      // Add block to dependency to respect block tags and ttl.
+      $response->addCacheableDependency($block);
+    }
+
+    $response->setContent($content);
+
+    return $response;
+  }
+  
 }
